0be7f3493411388f419f22732aa25c86d1317f3d
Alex Fila <fialex66@gmail.com>
2025-02-26 09:15:08 +0100

    Initial Commit

---
+++ Sipzy/ContentView.swift
@@ -0,0 +1,20 @@
+// ContentView.swift
+// (c) Alex Fila 26/02/25 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sipzy/SipzyApp.swift
@@ -0,0 +1,13 @@
+// SipzyApp.swift
+// (c) Alex Fila 26/02/25 
+
+import SwiftUI
+
+@main
+struct SipzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
