1380b9aeb4f8e7b27616be6fa847b67e6be7081a
Alex Fila <fialex66@gmail.com>
2025-02-26 10:37:27 +0100

    optimizing tabItems properties

--- Sipzy/ContentView.swift
+++ Sipzy/ContentView.swift
@@ -6,37 +6,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            CupsView()
-                .tabItem {
-                    Image(systemName: "wineglass")
-                        .font(.system(size: 35)) // Larger icon
-                        .foregroundColor(.white) // Ensure icon is really white
-                        .shadow(color: .black, radius: 15, x: 0, y: 15) // Add shadow effect
-                }
-            
-            MapView()
-                .tabItem {
-                    Image(systemName: "map")
-                        .font(.system(size: 35))
-                        .foregroundColor(.white)
-                        .shadow(color: .black, radius: 15, x: 0, y: 15) // Add shadow effect
-                }
-            
-            FavouriteView()
-                .tabItem {
-                    Image(systemName: "star")
-                        .font(.system(size: 35))
-                        .foregroundColor(.white)
-                        .shadow(color: .black, radius: 15, x: 0, y: 15) // Add shadow effect
-                }
-            
-            ProfileView()
-                .tabItem {
-                    Image(systemName: "person.crop.circle")
-                        .font(.system(size: 35))
-                        .foregroundColor(.white)
-                        .shadow(color: .black, radius: 15, x: 0, y: 15) // Add shadow effect
-                }
+            CupsView().tabItem { Image(systemName: "wineglass") }
+            MapView().tabItem { Image(systemName: "map") }
+            FavouriteView().tabItem { Image(systemName: "star") }
+            ProfileView().tabItem { Image(systemName: "person.crop.circle") }
         }
         .accentColor(.white) // This will change the color of the selected tab bar item to white
         .onAppear {
