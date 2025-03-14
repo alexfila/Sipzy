8dc48df03fad58e44b85b6126a599c56c4aa5e8d
Alex Fila <fialex66@gmail.com>
2025-03-14 09:11:22 +0100

    updating list and detail view

--- Sipzy/Pakilovejesus/ListOfClubs.swift
+++ Sipzy/Pakilovejesus/ListOfClubs.swift
@@ -9,74 +9,21 @@ import SwiftUI
 
 struct Contact: Identifiable {
     var id = UUID()
-    var name: String = "Pepi Vintage Room"
-    var phoneNumber: String = "+39 320 070 6622"
-    var address: String = "Vico San Domenico Maggiore 23, 80134 Napoli"
-    var link: URL? = URL(string: "https://www.instagram.com/pepivintageroom/")!
+    var name: String
+    var phoneNumber: String
+    var address: String
+    var link: URL?
 }
 
-struct Contact1: Identifiable {
-    var id = UUID()
-    var name: String = "Cammarota Spritz"
-    var phoneNumber: String = "+39 320 277 5687"
-    var address: String = "Vico Lungo Teatro Nuovo 31, 80134 Napoli"
-    var link: URL? = URL(string: "https://www.cammarotaspritz.it/")
-}
-
-struct Contact2: Identifiable {
-    var id = UUID()
-    var name: String = "Kestè"
-    var phoneNumber: String = "+39 346 494 9996"
-    var address: String = "Via San Giovanni Maggiore Pignatelli 26/27, 80134 Napoli"
-    var link: URL? = URL(string: "https://www.kestenapoli.com/")
-}
-struct Contact3: Identifiable {
-    var id = UUID()
-    var name: String = "Kitebeer"
-    var phoneNumber: String = "+39 081 177 66 308"
-    var address: String = "Vico Medina 15, 80123 Napoli"
-    var link: URL? = URL(string: "https://www.kitebeer.it/")
-}
-struct Contact4: Identifiable {
-    var id = UUID()
-    var name: String = "L'Antiquario"
-    var phoneNumber: String = "+39 081 7645390"
-    var address: String = "Via Vannella Gaetani 2, 80121 Napoli"
-    var link: URL? = URL(string: "https://lantiquarionapoli.superbexperience.com/")
-}
-struct Contact5: Identifiable {
-    var id = UUID()
-    var name: String = "Grand Tour cockatil boutique"
-    var phoneNumber: String = "+39 342 504 7868"
-    var address: String = "Via Santa Lucia 66/68, 80132 Napoli"
-    var link: URL? = URL(string: "https://lantiquarionapoli.superbexperience.com/")
-}
-struct Contact6: Identifiable {
-    var id = UUID()
-    var name: String = "Miami Barroom"
-    var phoneNumber: String = "+39 081 229 8332"
-    var address: String = "Via Raffaele Morgheen, 80129 Napoli"
-    var link: URL? = URL(string: "http://www.miamibarroom.it/")
-}
-struct Contact7: Identifiable {
-    var id = UUID()
-    var name: String = "La Fesseria"
-    var phoneNumber: String = "+39 081 1882 3409"
-    var address: String = "Via Giovanni Paladino 17, 80138 Napoli"
-    var link: URL? = URL(string: "https://lafesseria.it/")
-}
-struct Contact8: Identifiable {
-    var id = UUID()
-    var name: String = "Flanagan's Rooms"
-    var phoneNumber: String = "+39 375 512 2812"
-    var address: String = "Via Raffaele Morghen 31, 80127 Napoli"
-    var link: URL? = URL(string: "https://www.facebook.com/Flanagansrooms/")
-}
-struct Contact9: Identifiable {
-    var id = UUID()
-    var name: String = "Tasso Alcolico"
-    var phoneNumber: String = "+39 3485425373"
-    var address: String = "Via Girolamop Santa Croce 61, 80129 Napoli"
-    var link: URL? = URL(string: "https://www.facebook.com/tassoalcolicofans/?locale=it_IT")
-}
-
+let clubs: [Contact] = [
+    Contact(name: "Pepi Vintage Room", phoneNumber: "+39 320 070 6622", address: "Vico San Domenico Maggiore 23, 80134 Napoli", link: URL(string: "https://www.instagram.com/pepivintageroom/")),
+    Contact(name: "Cammarota Spritz", phoneNumber: "+39 320 277 5687", address: "Vico Lungo Teatro Nuovo 31, 80134 Napoli", link: URL(string: "https://www.cammarotaspritz.it/")),
+    Contact(name: "Kestè", phoneNumber: "+39 346 494 9996", address: "Via San Giovanni Maggiore Pignatelli 26/27, 80134 Napoli", link: URL(string: "https://www.kestenapoli.com/")),
+    Contact(name: "Kitebeer", phoneNumber: "+39 081 177 66 308", address: "Vico Medina 15, 80123 Napoli", link: URL(string: "https://www.kitebeer.it/")),
+    Contact(name: "L'Antiquario", phoneNumber: "+39 081 7645390", address: "Via Vannella Gaetani 2, 80121 Napoli", link: URL(string: "https://lantiquarionapoli.superbexperience.com/")),
+    Contact(name: "Grand Tour cocktail boutique", phoneNumber: "+39 342 504 7868", address: "Via Santa Lucia 66/68, 80132 Napoli", link: URL(string: "https://lantiquarionapoli.superbexperience.com/")),
+    Contact(name: "Miami Barroom", phoneNumber: "+39 081 229 8332", address: "Via Raffaele Morgheen, 80129 Napoli", link: URL(string: "http://www.miamibarroom.it/")),
+    Contact(name: "La Fesseria", phoneNumber: "+39 081 1882 3409", address: "Via Giovanni Paladino 17, 80138 Napoli", link: URL(string: "https://lafesseria.it/")),
+    Contact(name: "Flanagan's Rooms", phoneNumber: "+39 375 512 2812", address: "Via Raffaele Morghen 31, 80127 Napoli", link: URL(string: "https://www.facebook.com/Flanagansrooms/")),
+    Contact(name: "Tasso Alcolico", phoneNumber: "+39 3485425373", address: "Via Girolamo Santa Croce 61, 80129 Napoli", link: URL(string: "https://www.facebook.com/tassoalcolicofans/?locale=it_IT"))
+]
